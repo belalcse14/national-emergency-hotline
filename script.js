572038319dd1706c31cd767bc4cc0831a67495bb
Belal Hossain <belalcse2014@gmail.com>
2025-08-29 19:02:49 +0600

    History Section Functionality Added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let historyDataDisplay = [];
 // ==========================  Heart Count Added ==================================
 const heartCountDisplay = document.getElementById("heart");
 let heartCount = 0;
@@ -10,17 +11,44 @@ document.getElementById("heart-icon").addEventListener("click", function () {
 const coinCountDisplay = document.getElementById("coin");
 const serviceName = document.getElementById("service-name").innerText;
 const serviceNumber = document.getElementById("hotline-number").innerText;
+const historyContainer = document.getElementById("history-container");
+
 let coinCount = 100;
 
 document.getElementById("call-button").addEventListener("click", function () {
   coinCount = coinCount - 20;
   if (coinCount < 0) {
-    alert("Your Coin is Insufficient!");
+    alert("❌ Your Coin is Insufficient!");
     return;
   }
   alert(`📞 Calling.... \n ${serviceName} : ${serviceNumber}`);
 
   coinCountDisplay.innerText = coinCount;
+
+  const historyData = {
+    name: serviceName,
+    number: serviceNumber,
+    date: new Date().toLocaleTimeString(),
+  };
+  historyDataDisplay.push(historyData);
+
+  historyContainer.innerText = "";
+
+  for (const data of historyDataDisplay) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+              <div
+            class="bg-[#FAFAFA] mx-4 rounded-lg p-4 flex justify-between items-center mb-4"
+          >
+            <div>
+              <p>${data.name}</p>
+              <p>${data.number}</p>
+            </div>
+            <p>${data.date}</p>
+          </div>
+    `;
+    historyContainer.appendChild(div);
+  }
 });
 
 // ============================== Copy to Clipboard Added for Card #1 ==============================
@@ -35,3 +63,7 @@ document.getElementById("copy-button").addEventListener("click", function () {
 });
 
 // ================================= History Section Added =====================================
+document.getElementById("clear-button").addEventListener("click", function () {
+  historyDataDisplay = [];
+  historyContainer.innerText = "";
+});
